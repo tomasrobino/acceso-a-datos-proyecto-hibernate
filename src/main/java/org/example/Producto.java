daf4469131104c7190eda22edcda2c1f7bac9dcb
Tomas Robino <tomaserobino@gmail.com>
2026-01-12 12:01:23 +0100

    Producto y propiedades

---
+++ src/main/java/org/example/Producto.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String nombre;
+    @Column(nullable = false)
+    private Double precio;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
